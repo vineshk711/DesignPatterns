769376a68aed74cfad6435024b8daf54e670038b
Vinesh <vineshk711@gmail.com>
2024-01-13 16:42:03 +0530

    ADDED DOCUMENTATION FOR STRATEGY PATTERN

--- StrategyDesignPattern/src/Main.java
+++ StrategyDesignPattern/src/Main.java
@@ -1,15 +1,26 @@
 import driveStrategy.impl.DriveTruck;
+import driveStrategy.impl.DriveTwoWheeler;
 import vehicle.Vehicle;
 import vehicle.fourWheelerVehicle.Car;
 import vehicle.fourWheelerVehicle.Truck;
 import vehicle.twoWheelerVehicle.Bicycle;
 import vehicle.twoWheelerVehicle.Motorcycle;
 
+/**
+ * Strategy design pattern is one of the behavioral design pattern. Strategy pattern is used when we have multiple
+ * algorithm for a specific task and client decides the actual implementation to be used at runtime
+ * The Strategy pattern suggests that you take a class that does something specific in a lot of different
+ * ways and extract all of these algorithms into separate classes called strategies.
+ */
+
 public class Main {
     public static void main(String[] args) {
         Motorcycle motorcycle = new Motorcycle();
-        Vehicle bicycle = new Bicycle();
+        // decides implementation at runtime
+        Bicycle bicycle = new Bicycle(new DriveTwoWheeler());
         Car car = new Car();
+
+        // decides different implementation based on truck type
         Truck tataTruck = new Truck(new DriveTruck());
         Truck eicherTruck = new Truck();
         motorcycle.drive();
